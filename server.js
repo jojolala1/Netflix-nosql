5c39696bcf084107cf052dfa3bb1d8ea1b4dbfee
Thibau-Lasblei <lasthib.tl+1@gmail.com>
2025-12-18 15:30:38 +0100

    first route

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 const express = require('express')
 const mongoClient = require('mongodb').MongoClient
+
+const url  = 'mongodb://root:example@127.0.0.1:27017/?authSource=admin'
+
+const dbName = 'netflix-db';
+
+const app = express();
+
+let myNetflixDb = null;
+
+async function startServer() {
+    const clientMongo = new mongoClient(url);
+    await clientMongo.connect();
+    myNetflixDb = clientMongo.db(dbName);
+    app.listen(3000, ()=> {
+        console.log('Server is running on port 3000');
+    })
+}
+
+app.get('/allcontent', async (requestAnimationFrame, res) => {
+    try {
+        const filmsArray = await myNetflixDb.collection('movies').find({}).toArray();
+        res.json(filmsArray);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
